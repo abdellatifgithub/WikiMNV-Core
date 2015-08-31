9e0bf80c60de98589b64d4cfcc34c2e5563912e1
Thierry Lalandre <durstjack@gmail.com>
2015-08-31 12:24:38 +0200

    impleme

---
+++ WikiMNV-Core-Admin/src/main/java/fr/cap/wikimnv/service/impl/IServiceCRUD.java
@@ -0,0 +1,15 @@
+package fr.cap.wikimnv.service.impl;
+
+import java.util.Map;
+
+import fr.cap.wikimnv.core.commons.ICRUDGeneric;
+import fr.cap.wikimnv.core.pojo.Profil;
+import fr.cap.wikimnv.core.pojo.Query;
+
+public interface IServiceCRUD extends ICRUDGeneric{
+
+	int faireRequete(Query profilRanking, Map<String, Object> params);
+
+	void sauver(Profil profil);
+
+}
