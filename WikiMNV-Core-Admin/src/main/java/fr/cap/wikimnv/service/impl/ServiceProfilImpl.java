a2710afb8e78b8a48d18dcbe2c9b9e44a64492a5
Thierry Lalandre <durstjack@gmail.com>
2015-08-31 11:05:13 +0200

    progress in serviceProfil

--- WikiMNV-Core-Admin/src/main/java/fr/cap/wikimnv/service/impl/ServiceProfilImpl.java
+++ WikiMNV-Core-Admin/src/main/java/fr/cap/wikimnv/service/impl/ServiceProfilImpl.java
@@ -1,88 +1,69 @@
 package fr.cap.wikimnv.service.impl;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-
+import fr.cap.wikimnv.core.commons.exception.MNVException;
+import fr.cap.wikimnv.core.pojo.Profil;
+import fr.cap.wikimnv.core.pojo.Query;
 import fr.cap.wikimnv.service.IServiceProfil;
 
-
-
 public class ServiceProfilImpl implements IServiceProfil {
-	
-	//Besoin du service persistence
-	IServiceCRUD icrud;
-	
-	
-	public IServiceCRUD getIcrud() { return icrud; }
-	public void setIcrud(IServiceCRUD icrud) { this.icrud = icrud; }
 
-	//constructeur
-	public ServiceProfilImpl() {
-		super();		
+	// Besoin du service persistence
+	IServiceCRUD icrud;
+
+	public IServiceCRUD getIcrud() {
+		return icrud;
 	}
 
-	@Override
-	/**
-	 * countArticlesEcris()
-	 * Methode qui renvoi le nb article ecrits par un profil<br/>
-	 */
+	public void setIcrud(IServiceCRUD icrud) {
+		this.icrud = icrud;
+	}
+
+	// constructeur
+	public ServiceProfilImpl() {
+		super();
+	}
+
 	public int countArticlesEcris(Profil p) throws MNVException {
-		
+
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("pId", p.getId());
 
-		return (int) icrud.faireRequete(Query.PROFIL_COUNTARTICLESECRIT, params);
+		return (int) icrud
+				.faireRequete(Query.PROFIL_COUNTARTICLESECRIT, params);
 	}
 
-	@Override
-	/**
-	 * ranking()
-	 * Methode qui renvoi la note la position d'un profil
-	 */
-	public int ranking(Profil p) throws MNVException {		
-		Map<String, Object> params=new HashMap<String, Object>();
+	public int ranking(Profil p) throws MNVException {
+		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("pId", p.getId());
 
-		
-		return (int) icrud.faireRequete(Query.PROFIL_RANKING,params);	
+		return (int) icrud.faireRequete(Query.PROFIL_RANKING, params);
 	}
 
-	@Override
-	/**
-	 * updateAvatar()
-	 * Methode permettant de mettre à jour l'avatar
-	 */
-	public void updateAvatar(String urlAvatar, Profil profil) throws MNVException {
-			profil.setAvatar(urlAvatar);
-			icrud.sauver(profil);	
+	public void updateAvatar(String urlAvatar, Profil profil)
+			throws MNVException {
+		profil.setAvatar(urlAvatar);
+		icrud.sauver(profil);
 	}
 
-	@Override
 	public Set<?> lister(Class cls) throws MNVException {
 		// TODO Auto-generated method stub
 		return icrud.lister(cls);
 	}
 
-	@Override
 	public Object lire(Object obj, Class cls) throws MNVException {
 		// TODO Auto-generated method stub
 		return icrud.lire(obj, cls);
 	}
 
-	@Override
-	public Object supprimer(Object obj)  throws MNVException{
+	public Object supprimer(Object obj) throws MNVException {
 		// TODO Auto-generated method stub
 		return icrud.supprimer(obj);
 	}
 
-	@Override
 	public Object sauver(Object obj) throws MNVException {
 		// TODO Auto-generated method stub
 		return icrud.sauver(obj);
