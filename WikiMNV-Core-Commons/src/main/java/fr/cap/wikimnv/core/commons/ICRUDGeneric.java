4effa61f41a04da9eb2f26cc8111535e9405c257
abdellatifgithub <abdellatif.mba@gmail.com>
2015-08-31 11:36:11 +0200

    mise a jours TypeStructure

--- WikiMNV-Core-Commons/src/main/java/fr/cap/wikimnv/core/commons/ICRUDGeneric.java
+++ WikiMNV-Core-Commons/src/main/java/fr/cap/wikimnv/core/commons/ICRUDGeneric.java
@@ -3,11 +3,12 @@ package fr.cap.wikimnv.core.commons;
 import java.util.Set;
 
 import fr.cap.wikimnv.core.commons.exception.MNVException;
+import fr.cap.wikimnv.core.pojo.TypeStructure;
 
 public interface ICRUDGeneric  {
 	
-	Set<?> lister(Class cls) throws MNVException ;
-	Object lire(Object obj, Class cls)  throws MNVException;
+	Set<?> lister(TypeStructure type) throws MNVException ;
+	Object lire(Object obj, TypeStructure type)  throws MNVException;
 	Object supprimer(Object obj)  throws MNVException;
 	Object sauver(Object obj) throws MNVException ;
 	
