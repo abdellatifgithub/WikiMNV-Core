46c459a1f1a7bf966dce1078f5de89b2796d23e3
Thierry Lalandre <durstjack@gmail.com>
2015-09-02 13:55:27 +0200

    ajout des nvlles versions de lib

--- WikiMNV-Core-Admin/src/main/java/fr/cap/wikimnv/service/impl/ServiceUserImpl.java
+++ WikiMNV-Core-Admin/src/main/java/fr/cap/wikimnv/service/impl/ServiceUserImpl.java
@@ -1,8 +1,11 @@
 package fr.cap.wikimnv.service.impl;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import fr.cap.wikimnv.core.commons.exception.MNVException;
+import fr.cap.wikimnv.core.persistance.MNVException_Exception;
+import fr.cap.wikimnv.core.persistance.cli.PersistanceCli;
 import fr.cap.wikimnv.core.pojo.TypeStructure;
 import fr.cap.wikimnv.core.pojo.User;
 import fr.cap.wikimnv.service.IServiceUser;
@@ -10,33 +13,57 @@ import fr.cap.wikimnv.service.IServiceUser;
 
 public class ServiceUserImpl implements IServiceUser {
 
-	// Besoin du service persistence
-	IServiceCRUD icrud;
+	// Besoin du service persistence (CRUD notamment)
+	PersistanceCli pcli;
 
 	//getters et setter pour IOC Spring
-	public IServiceCRUD getIcrud() { return icrud; }
-	public void setIcrud(IServiceCRUD icrud) { this.icrud = icrud; }
-	
+	public PersistanceCli getPcli() { return pcli; }
+	public void setPcli(PersistanceCli pcli) {	this.pcli = pcli; }
+		
 	
 	public void bannir(Object id) {
 		User u = (User)id;
 		u.setBannit(true);		
 	}
 
-	public Object lire(Object obj, TypeStructure type) throws MNVException {
-		return icrud.lire(obj, type);
+	public Object lire(Object obj, TypeStructure type) {
+
+			try {
+				return pcli.lire(obj, type);
+			} catch (MNVException_Exception e) {
+				e.printStackTrace();
+				return null;
+			}
+
 	}
 
-	public Set<?> lister(TypeStructure type) throws MNVException {
-		return icrud.lister(type);
+	public Set<?> lister(TypeStructure type){
+		try {
+			//cast en Set
+			return (Set<?>)pcli.lister(type);
+		} catch (MNVException_Exception e) {
+			e.printStackTrace();
+			//renvoi Set vide
+			return new HashSet<String>();
+		}
 	}
 
-	public Object sauver(Object obj) throws MNVException {
-		return icrud.sauver(obj);
+	public Object sauver(Object obj) {
+		try {
+			return pcli.sauver(obj);
+		} catch (MNVException_Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
-	public Object supprimer(Object obj) throws MNVException {
-		return icrud.supprimer(obj);
+	public Object supprimer(Object obj){
+		try {
+			return pcli.supprimer(obj);
+		} catch (MNVException_Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 
