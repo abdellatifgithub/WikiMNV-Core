10d283c726d4f29b1c242d95c510ecd88d014d5e
jcham01 <julien.chameroy@gmail.com>
2015-09-03 09:38:58 +0200

    modification de set en list

--- WikiMNV-Core-Commons/src/main/java/fr/cap/wikimnv/core/pojo/Template.java
+++ WikiMNV-Core-Commons/src/main/java/fr/cap/wikimnv/core/pojo/Template.java
@@ -1,18 +1,26 @@
 package fr.cap.wikimnv.core.pojo;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 public class Template {
 	String nom;
-	Set<String> meta;
+	List<String> meta;
 	
-	public Template(String nom) {
+	public Template(String nom, List<String> meta) {
+		this.nom = nom;
+		this.meta = meta;
+	}
+	
+	public List<String> getMeta() {
+		return meta;
+	}
+	
+	/*public Template(String nom) {
 		this.nom=nom;
 		this.meta= new HashSet<String>();
 	}
 	
 	public Set<String> getMeta() {
 		return meta;
-	}
+	}*/
 }
